f08c21380e4d5494733fcb153bb6fc27a683c073
drping <cedric.ogire@epitech.eu>
2018-09-26 11:04:48 +0200

    [UI] : add qt main window but the code is not call yet

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -6,8 +6,15 @@
 */
 
 #include <iostream>
+#include <QApplication>
+#include "mainwindow.h"
 
 int main(int argc, char * const *argv)
 {
+	/* QApplication a(argc, argv);
+	MainWindow w;
+	w.show();
+
+	return a.exec(); */
 	return EXIT_SUCCESS;
 }
