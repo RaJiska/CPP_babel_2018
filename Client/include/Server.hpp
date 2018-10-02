396e0d673912fe66996d74f4233538636f7792c5
Ra'Jiska <dodo.lasticot@gmail.com>
2018-10-02 20:08:40 +0200

    Client Base TCP Socket

---
+++ Client/include/Server.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2018
+** Server
+** File description:
+** Server
+*/
+
+#ifndef SERVER_HPP_
+	#define SERVER_HPP_
+
+#include <QtNetwork>
+#include "NetworkMessage.hpp"
+
+class Server {
+	public:
+	Server(const std::string &address, uint16_t port);
+	~Server();
+
+	bool sendLoginMsg(const std::string &name) noexcept;
+
+	private:
+	void sendMessage(NetworkMessage &msg) noexcept;
+	void readMessage() noexcept;
+
+	QTcpSocket socket;
+	unsigned long long int clientId;
+	NetworkMessage response;
+};
+
+#endif /* !SERVER_HPP_ */
---
+++ Client/src/Server.cpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2018
+** Server
+** File description:
+** Server
+*/
+
+#include <string>
+#include "Server.hpp"
+
+Server::Server(const std::string &address, uint16_t port)
+{
+	this->socket.connectToHost(QString::fromStdString(address), port);
+	if (!this->socket.waitForConnected())
+		throw std::logic_error("Connection timeout");
+}
+
+Server::~Server()
+{
+	this->socket.disconnect();
+}
+
+bool Server::sendLoginMsg(const std::string &name) noexcept
+{
+	NetworkMessage msg;
+	struct NetworkMessage::Header &header = msg.getHeader();
+	header.from = 0;
+	header.to = 0;
+	header.type == NetworkMessage::Header::MessageType::TYPE_LOGIN;
+	msg.setData((const unsigned char *) name.c_str(), 32);
+	this->sendMessage(msg);
+}
+
+void Server::sendMessage(NetworkMessage &msg) noexcept
+{
+	struct NetworkMessage::Header header = msg.getHeader();
+	this->socket.write((const char *) &header,
+		sizeof(struct NetworkMessage::Header));
+	this->socket.write((const char *) msg.getData(), msg.getHeader().size);
+	this->socket.waitForBytesWritten
+}
+
+void Server::readMessage(NetworkMessage &msg) noexcept
+{
+
+}
